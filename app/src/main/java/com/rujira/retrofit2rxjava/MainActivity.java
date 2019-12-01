4846de0e5d6a92026d91a05a6fb8e3f1f990550b
Rujira Kongsomran <biwberry@outlook.com>
2019-12-01 20:40:18 +0700

    Fetch API and set data for RecyclerView

--- app/src/main/java/com/rujira/retrofit2rxjava/MainActivity.java
+++ app/src/main/java/com/rujira/retrofit2rxjava/MainActivity.java
@@ -1,14 +1,69 @@
 package com.rujira.retrofit2rxjava;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.rujira.retrofit2rxjava.Adapter.PostAdapter;
+import com.rujira.retrofit2rxjava.Model.Post;
+import com.rujira.retrofit2rxjava.Retrofit.IMyAPI;
+import com.rujira.retrofit2rxjava.Retrofit.RetrofitClient;
+
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
+
 public class MainActivity extends AppCompatActivity {
+
+    IMyAPI myAPI;
+    RecyclerView recycler_posts;
+    CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Init API
+        Retrofit retrofit = RetrofitClient.getInstance();
+        myAPI = retrofit.create(IMyAPI.class);
+
+        // View
+        recycler_posts = (RecyclerView) findViewById(R.id.recycler_posts);
+        recycler_posts.setHasFixedSize(true);
+        recycler_posts.setLayoutManager(new LinearLayoutManager(this));
+
+        fetchData();
+
+    }
+
+    private void fetchData() {
+        compositeDisposable.add(myAPI.getPosts()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<List<Post>>() {
+                    @Override
+                    public void accept(List<Post> posts) throws Exception {
+                        displayData(posts);
+
+                    }
+                }));
+    }
+
+    private void displayData(List<Post> posts) {
+        PostAdapter adapter = new PostAdapter(this, posts);
+        recycler_posts.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStop() {
+        compositeDisposable.clear();
+        super.onStop();
     }
 }
